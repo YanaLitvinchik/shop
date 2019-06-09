ea9a3f3890117184d22aca58669ba52afb978f72
Яна Литвинчик <yana.litvinchik@gmail.com>
2019-06-09 02:45:09 +0200

    fix cards

--- Shop0.1/Controllers/GoodController.cs
+++ Shop0.1/Controllers/GoodController.cs
@@ -10,7 +10,7 @@ namespace Shop0._1.Controllers
     public class GoodController : Controller
     {
         private IGoodRepository repository;
-        public int PageSize = 4;
+        public int PageSize = 8;
         public GoodController(IGoodRepository goodRepository)
         {
             this.repository = goodRepository;
